earthians/marley

fix: module import path

healthcare/patches/v0_0/setup_service_request_and_insurance.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 import frappe
-from healthcare.healthcare.setup import setup_service_request_masters, create_customer_groups
+from healthcare.setup import setup_service_request_masters, create_customer_groups
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 from healthcare.healthcare.healthcare import data
 

Tacten/biograph

healthcare/patches/v0_0/setup_service_request_and_insurance.py
@@ -4,7 +4,7 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 from healthcare.healthcare.healthcare import data
-from healthcare.healthcare.setup import create_customer_groups, setup_service_request_masters
+from healthcare.setup import create_customer_groups, setup_service_request_masters
 
 
 def execute():