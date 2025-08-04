c41288bb6358cb4d3bb7cfb4062950b8a059bdb6
Anoop Kurungadam <anoop@earthianslive.com>
2025-08-04 21:15:46 +0530

    fix: broken build fixes in vite.config.js remove unavailable corenerstone package
    Co-authored-by: Hussain <buildwithhussain@gmail.com>

--- viewer/vite.config.js
+++ viewer/vite.config.js
@@ -1,24 +1,36 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import Icons from 'unplugin-icons/vite'
+import frappeui from "frappe-ui/vite"
 import Components from 'unplugin-vue-components/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig({
-	base: "/assets/healthcare/dcmviewer/",
 	plugins: [
 		vue(),
 		Components({
 			resolvers: [IconsResolver({ prefix: "" })],
+		}),
+		frappeui({
+			frappeProxy: true,
+			jinjaBootData: true,
+			lucideIcons: true,
+			buildConfig: {
+				indexHtmlPath: "../healthcare/www/viewer.html",
+				emptyOutDir: true,
+				sourcemap: true,
+			},
 		}),
 		Icons({
 			compiler: "vue3",
 		}),
 	],
 	build: {
-		outDir: "../public/dcmviewer",
+		chunkSizeWarningLimit: 1500,
+		outDir: "../healthcare/public/viewer",
+		emptyOutDir: true,
+		target: "es2015",
 		sourcemap: true,
-		assetsDir: ".",
 	},
 	worker: {
 		format: "es",
